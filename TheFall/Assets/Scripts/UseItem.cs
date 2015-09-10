920e64d39f84dcc044d8be77e8a213545f4cff16
Art <voxatrophia@gmail.com>
2015-09-10 02:23:00 -0700

    Character can pick up items, they are viewable in UI. Can use items (no functionality yet) and the image is removed. Need to implement item functionality.

---
+++ TheFall/Assets/Scripts/UseItem.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UseItem : MonoBehaviour {
+
+	GameObject item;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButtonDown("Fire1") ){
+			item = ItemManager.i.GetItem();
+			if(item != null){
+				switch(item.name){
+					case "Bomb":
+						Bomb useBomb = item.GetComponent<Bomb>();
+						useBomb.Explode();
+						break;
+					case "Apple":
+						Apple useApple = item.GetComponent<Apple>();
+						useApple.Health();
+						break;
+				}
+				//Destroy item (or recycle for object pooling)
+				ItemManager.i.UsedItem();
+
+
+			}
+			else {
+				Debug.Log("No Item");
+			}
+		}
+	}
+}
