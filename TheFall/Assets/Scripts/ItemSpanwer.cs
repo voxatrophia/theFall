f20748f2fb5d02a963c6aba54c18f3b2960287d2
Art <voxatrophia@gmail.com>
2015-12-17 17:25:47 -0800

    Starting Tension system. Added weight to item spawning, that can be adjusted depending on tension level.

--- TheFall/Assets/Scripts/ItemSpanwer.cs
+++ TheFall/Assets/Scripts/ItemSpanwer.cs
@@ -1,29 +1,105 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-[RequireComponent (typeof(MultiObjectPooler))]
-public class ItemSpanwer : MonoBehaviour {
-	public float spawnMinTime = 3f;
-	public float spawnMaxTime = 5f;
+[RequireComponent(typeof(MultiObjectPooler))]
+public class ItemSpanwer : MonoBehaviour
+{
+    public float spawnMinTime = 3f;
+    public float spawnMaxTime = 5f;
 
-	Animator anim;
-	MultiObjectPooler itemList;
+    Dictionary<string, float> items;
 
-	void Start() {
-		itemList = GetComponent<MultiObjectPooler>();
+    Animator anim;
+    MultiObjectPooler itemList;
+    string itemToSpawn;
 
-		StartCoroutine(SpawnItem());
-	}
+    void Start()
+    {
+        itemList = GetComponent<MultiObjectPooler>();
+        SetUpItemList();
+        StartCoroutine(SpawnRandomItem());
+    }
 
-	IEnumerator SpawnItem(){
-		while(true){
-			yield return Yielders.Get(Random.Range(spawnMinTime, spawnMaxTime));
-			GameObject item = itemList.GetPooledObjectOfRandomType();
+    void OnDisable()
+    {
+        //        Debug.Log(itemToSpawn);
+    }
+
+    void SetUpItemList()
+    {
+        items = new Dictionary<string, float>();
+        foreach (string itemType in itemList.GetObjectTypes())
+        {
+            items[itemType] = 0.25f;
+        }
+        items["NA"] = 0.25f;
+    }
+
+    void SpawnItem()
+    {
+        //Set up list (already done in start method, only needed once)
+        //get tension measurements (tension y, health, tension x)
+        //Assign new probabilities based on tension
+        //spawn item of correct type (or none at all)
+        if (Health.CheckHealth() < 2) {
+            items["Apple"] += 0.5f;
+        }
+        itemToSpawn = ChooseItem();
+        Debug.Log(itemToSpawn);
+        if (itemToSpawn == "NA") {
+            return;
+        }
+        GameObject item = itemList.GetPooledObject(itemToSpawn);
+        if (item != null)
+        {
+            item.transform.position = new Vector3(Random.Range(-10, 10), transform.position.y, transform.position.z);
+            item.transform.rotation = Quaternion.identity;
+            item.SetActive(true);
+
+        }
+    }
+
+    string ChooseItem()
+    {
+        float total = 0;
+
+        foreach (KeyValuePair<string, float> elem in items)
+        {
+            // do something with entry.Value or entry.Key
+            total += elem.Value;
+        }
+
+        float randomPoint = Random.value * total;
+
+        foreach (KeyValuePair<string, float> elem in items)
+        {
+            if (randomPoint < elem.Value)
+            {
+                return elem.Key;
+            }
+            else
+            {
+                randomPoint -= elem.Value;
+            }
+        }
+        return "NA";
+    }
+
+    IEnumerator SpawnRandomItem()
+    {
+        while (true)
+        {
+            yield return Yielders.Get(Random.Range(spawnMinTime, spawnMaxTime));
+            SpawnItem();
+            /*
+            GameObject item = itemList.GetPooledObjectOfRandomType();
 			if(item != null){
 				item.transform.position = new Vector3(Random.Range(-10,10), transform.position.y, transform.position.z);
 				item.transform.rotation = Quaternion.identity;
 				item.SetActive(true);
 			}
-		}
-	}
+           */
+        }
+    }
 }
---
+++ TheFall/Assets/TensionManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TensionManager : Singleton<TensionManager> {
+    //Track tension in the game
+    //Tension builds slowly over time (when above 0 on Y axis)
+    //Using items reduces tension (drastically)
+    //Track: Y, X, Health, attack avoidance streak
+
+    public Transform player;
+    public float tensionY = 0;
+
+
+    void Start () {
+	
+	}
+
+    void OnDisable() {
+        Debug.Log("Tension Y:" + tensionY);
+    }
+
+    void Tutorial() {
+        //Remove boss
+        //Remove item spawning
+        //Remove energy charging
+        //Remove score tracking
+    }
+
+        void Update() {
+            if (player.position.y > 0) {
+                tensionY += Time.deltaTime;
+            }
+
+        }
+    }
